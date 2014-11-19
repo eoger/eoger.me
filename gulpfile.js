3b848726fc28f9e19f9abf85fecd46b1bcc5a09a
Edouard Oger <edouard.oger@gmail.com>
2014-11-19 16:57:33 +0100

    Use gulp-less instead of css files directly

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('less', function () {
+  gulp.src('./less/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./public/css'));
 });
+
+gulp.task('default', ['less']);
