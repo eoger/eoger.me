0a01435bea477f0e5be2fcab69180267fca5f958
Edouard Oger <edouard.oger@gmail.com>
2014-11-23 16:04:30 +0100

    Use watchify + Remove bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var path = require('path');
 var less = require('gulp-less');
 var uglify = require('gulp-uglify');
 var browserify = require('browserify');
+var watchify = require('watchify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 var streamify = require('gulp-streamify');
@@ -27,18 +29,34 @@ gulp.task('css', function () {
     .pipe(gulp.dest(build.css));
 });
 
+function bundlejs(watch) {
+  var bundler = browserify(src.main_js, watchify.args);
+  if(watch) {
+    bundler = watchify(bundler);
+  }
+ 
+  bundler.transform(reactify);
+ 
+  var rebundle = function() {
+    gutil.log('Start browserify');
+    var stream = bundler.bundle();
+    stream.on('error', gutil.log.bind(gutil, 'Browserify error'));
+    stream.on('end', gutil.log.bind(gutil, 'End browserify'));
+    stream = stream.pipe(source(build.main_js));
+    return stream.pipe(gulp.dest(build.js));
+  };
+ 
+  bundler.on('update', rebundle);
+  return rebundle();
+}
+
 gulp.task('js', function() {
-  return browserify(src.main_js)
-    .transform(reactify)
-    .bundle()
-    .pipe(source(build.main_js))
-    .pipe(streamify(uglify()))
-    .pipe(gulp.dest(build.js));
+  return bundlejs(false);
 });
 
 gulp.task('watch', function() {
   gulp.watch(src.less, ['css']);
-  gulp.watch(src.js, ['js']);
+  return bundlejs(true);
 });
 
 gulp.task('default', ['css', 'js']);
